2b8b62f57bb6c7d1e014e588b2e3ff173085594c
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-23 16:41:40 +0900

    ページの表示のためのカードを作成

---
+++ src/components/TodoCard.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div class="div-card"></div>
+</template>
+
+<style scoped>
+.div-card {
+  margin-top: 10px;
+  padding: 150px;
+  background: white;
+  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.1);
+}
+</style>
