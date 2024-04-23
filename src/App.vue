6b950f4d0a41b36c6966983490d4b37da0ed2aaf
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-23 18:11:13 +0900

    基本的なカート上の表示を作成するため

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,59 @@
 <script setup>
-import MemoCard from './components/MemoCard.vue'
+import { ref } from 'vue'
+
+const isDisplayed = ref(false)
+
+const toggleDisplay = () => {
+  isDisplayed.value = !isDisplayed.value
+}
 </script>
 
 <template>
-  <MemoCard>
-    <template v-slot:memo-links>
-      <h1>link</h1>
-    </template>
-  </MemoCard>
+  <div class="card">
+    <div class="links-wrapper">
+      <a @click="toggleDisplay">+</a>
+    </div>
+    <div class="form-wrapper" v-if="isDisplayed">
+      <form>
+        <textarea value="新規メモ" placeholder="メモを入力してください" required />
+        <div class="btns-wrapper">
+          <button type="submit">更新</button>
+          <button type="button">削除</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.card {
+  display: flex;
+  justify-content: space-between;
+  height: 500px;
+  width: 500px;
+  background: white;
+  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.1);
+  padding: 10px;
+}
+
+a {
+  text-decoration: none;
+  display: inline;
+  font-size: 20px;
+  cursor: pointer;
+}
+
+textarea {
+  height: 400px;
+  border-radius: 10px;
+}
+
+.links-wrapper {
+}
+
+.form-wrapper {
+}
+
+.btns-wrapper {
+}
+</style>
---
+++ src/components/MemoLink.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <a href="#"><slot>Default Link Text</slot></a>
+</template>
+
+<style scoped>
+a {
+  text-decoration: none;
+  display: inline;
+  font-size: 20px;
+}
+</style>
