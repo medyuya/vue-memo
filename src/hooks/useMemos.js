1151007ef86f3f86381a61dcf025fa8b4ab1c7d5
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-05-02 10:04:14 +0900

    localStorageのキーに関して、同じ値が複数箇所で現れるため定数で定義し直したため

--- src/hooks/useMemos.js
+++ src/hooks/useMemos.js
@@ -2,7 +2,9 @@ import { ref } from 'vue'
 import * as localStorageHandlers from '../utils/localStorageHandlers.js'
 
 export const useMemos = () => {
-  const memos = ref(localStorageHandlers.get('memos'))
+  const MEMOS_STORAGE_KEY = 'memos'
+
+  const memos = ref(localStorageHandlers.get(MEMOS_STORAGE_KEY))
 
   const addNewMemo = (newText) => {
     if (newText.trim() === '') {
@@ -16,20 +18,20 @@ export const useMemos = () => {
       content: newText
     }
 
-    localStorageHandlers.create('memos', newMemo)
+    localStorageHandlers.create(MEMOS_STORAGE_KEY, newMemo)
 
     memos.value.push(newMemo)
   }
 
   const removeMemo = (targetId) => {
-    localStorageHandlers.remove('memos', targetId)
+    localStorageHandlers.remove(MEMOS_STORAGE_KEY, targetId)
 
     const indexToRemove = memos.value.findIndex((memo) => memo.id === targetId)
     memos.value.splice(indexToRemove, 1)
   }
 
   const updateMemo = (targetId, updateText) => {
-    localStorageHandlers.update('memos', targetId, updateText)
+    localStorageHandlers.update(MEMOS_STORAGE_KEY, targetId, updateText)
 
     memos.value = memos.value.map((memo) => {
       if (memo.id === targetId) {
